6ef25592259faab11d0fbb630bfedd06b65cae62
FFGMP <ffgmp@users.noreply.github.com>
2023-09-15 00:40:02 +0100

    Update on the about me page

--- src/app/components/leftCollumn.js
+++ src/app/components/leftCollumn.js
@@ -1,12 +1,11 @@
 const CardForAbouts = ({ SVG, Title, SubTitle }) => (
     <div className="grid grid-flow-col gap-x-4 justify-center items-center mb-7 max-w-[212.85px]">
-        <div className="row-span-2 p-2 rounded-md bg-gradient-to-l from-neutral-900 to-neutral-950">
+        <div className="shadow-md shadow-neutral-800 row-span-2 p-2 rounded-md bg-gradient-to-l from-neutral-900 to-neutral-950">
             {SVG}
         </div>
         <div className="col-span-5">{Title}</div>
         <div className="col-span-5 min-w-[150.85px] block">{SubTitle}</div>
     </div>
-
 );
 
 export function LeftCollumn() {
@@ -28,6 +27,7 @@ export function LeftCollumn() {
                 Title={<p className="text-xs">EMAIL</p>}
                 SubTitle={<a className="hover:underline hover:text-blue-300 visited:text-purple-300" href="mailto:ffgmp@hotmail.com"><p className="text-sm">ffgmp@hotmail.com</p></a>}
             />
+
             <CardForAbouts SVG={<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>}
                 Title={<p className="text-xs">LINKEDIN</p>}
                 SubTitle={<a className="hover:underline hover:text-blue-300 visited:text-purple-300" href="https://linkedin.com/ffgmp"><p className="text-sm">linkedin.com/ffgmp</p></a>}
